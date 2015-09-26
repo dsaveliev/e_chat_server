3c1e467387528abb44c0839f2d6bd638b20855a8
Dmitry Saveliev <d.e.saveliev@gmail.com>
2015-09-26 20:26:58 +0300

    Add simple logging

--- src/handlers/e_chat_server_chat_handler.erl
+++ src/handlers/e_chat_server_chat_handler.erl
@@ -19,6 +19,7 @@ init(_, _, _) ->
     {upgrade, protocol, cowboy_websocket}.
 
 websocket_init(_, Req, _Opts) ->
+    erlang:display(">>>>>> [WEBSOCKET] Start init connection"),
     self() ! post_init,
 
     {SessionId, _} = cowboy_req:qs_val(<<"session_id">>, Req, undefined),
@@ -28,11 +29,15 @@ websocket_init(_, Req, _Opts) ->
     {Room, _Users} = e_chat_server_search_room_service:perform(RoomId, User),
 
     Req2 = cowboy_req:compact(Req),
+    erlang:display(">>>>>> [WEBSOCKET] User#id: " ++ integer_to_list(User#user.id)),
+    erlang:display(">>>>>> [WEBSOCKET] Room#id: " ++ integer_to_list(Room#room.id)),
+    erlang:display(">>>>>> [WEBSOCKET] Finish init connection"),
     {ok, Req2, #state{room = Room, user = User}}.
 
 %%%% Получение сообщения
 websocket_handle({text, Data}, Req, State = #state{room_pid = RoomPid, user = User}) ->
     Text = fetch_message(Data),
+    erlang:display(lists:flatten(">>>>>> [WEBSOCKET] Message received: ", Text)),
     gen_server:cast(RoomPid, {forward_message, Text, User#user.id, self()}),
     {ok, Req, State};
 websocket_handle({binary, _Data}, Req, State) ->
@@ -52,6 +57,7 @@ websocket_info({add_room, RoomPid}, Req, State) ->
     {ok, Req, State#state{room_pid = RoomPid}};
 %%%% Отправка сообщения
 websocket_info({send_message, Message}, Req, State) ->
+    erlang:display(lists:flatten(">>>>>> [WEBSOCKET] Message sent: ", Message)),
     {reply, {text, message_to_json(Message)}, Req, State};
 websocket_info(_Info, Req, State) ->
     {ok, Req, State}.
